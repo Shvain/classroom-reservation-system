9ed5d367e36881c93d5bd581f20218545f4aaaaf
Shvain <takuto3987@gmail.com>
2024-06-13 09:40:21 +0900

    mavenファイル構築

---
+++ src/ReservationInformation.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class ReservationInformation {
+    
+}
